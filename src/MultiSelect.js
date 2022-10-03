4ee74c4cd7dea91946f41d0357f01ca934dca9f9
Peter Kok <kretep@users.noreply.github.com>
2022-10-03 17:41:24 +0200

    Fix checkbox layout
    ty @ctwhome <3

--- src/MultiSelect.js
+++ src/MultiSelect.js
@@ -32,11 +32,17 @@ const MultiSelect = ({ options, label, selection, onChanged }) => {
       </div>
       <input className="placeholder:italic placeholder:text-slate-400 block bg-white w-full border border-slate-300 rounded-md py-2 pl-3 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm"
         type="search" placeholder="Filter..." onChange={e => setFilter(e.target.value)} ></input>
-      <ul tabIndex="0" className="text-left">
+      <ul tabIndex="0">
         { Object.keys(options).map(key => (
-          (filter === "" || options[key].toLowerCase().includes(lowerCaseFilter)) && <li key={key}><label>
-            <input type="checkbox" className="checkbox" checked={selection.includes(key)}
-             onChange={event => updateSelection(key, event.target.checked)}/>{options[key]}</label></li>
+          (filter === "" || options[key].toLowerCase().includes(lowerCaseFilter)) && <li key={key} className="flex">
+              <div className="form-control">
+                <label className="label cursor-pointer p-0.5 gap-1">
+                  <input type="checkbox" className="checkbox" checked={selection.includes(key)}
+                          onChange={event => updateSelection(key, event.target.checked)}/>
+                  <span className="label-text">{options[key]}</span>
+                </label>
+              </div>
+            </li>
         )) }
       </ul>
     </div>
